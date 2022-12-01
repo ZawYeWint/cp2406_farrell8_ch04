a753b397f0d9ff1c09fea25929b83edbe1e178df
ZawYeWint <zawyewint@my.jcu.edu.au>
2022-12-01 17:12:29 +0800

    Fix Debugging

---
+++ DebuggingExercises/FixDebugFour1.java
@@ -0,0 +1,22 @@
+// This program assigns values to two variables
+// and performs mathematical operations with them
+public class FixDebugFour1
+{
+   public static void main(String args[])
+   {
+      int x = 5;
+      int y = 8;
+      int a = x+y;
+      System.out.println("adding " + a);
+
+      int z = 19;
+      int b = z-y;
+      System.out.println("subtracting "  + b);
+
+      int c = z/x;
+      System.out.println("dividing " + c);
+
+      int d = x*z;
+      System.out.println("multiplying " + d);
+   }
+}
